50b7d6f0f03728d2388857316e88cb9681f977c5
Felipe Prenholato <philipe.rp@gmail.com>
2015-02-25 09:39:39 -0300

    Damm tests fixed.

--- test_settings.py
+++ test_settings.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
 import sys
+
+class DisableMigrations(dict):
+
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return "notmigrations"
+
 
 gettext = lambda s: s
 HELPER_SETTINGS = {
@@ -61,15 +71,24 @@ HELPER_SETTINGS = {
         'easy_thumbnails.processors.filters',
     ),
     # 'EMAIL_BACKEND': 'django.core.mail.backends.locmem.EmailBackend',
-    'MIGRATION_MODULES ': {
-        'filer': 'filer.migrations_django',
-    },
+    # 'MIGRATION_MODULES ': {
+    #     'filer': 'filer.migrations_django',
+    # },
+    'MIGRATION_MODULES': DisableMigrations(),
     'SOUTH_TESTS_MIGRATE': False,
     # 'DEBUG': True,
     # 'TEMPLATE_DEBUG': True,
     'ALDRYN_EVENTS_USER_REGISTRATION_EMAIL': True
 }
 
+# if 'test' in sys.argv:
+#     HELPER_SETTINGS['DATABASES'] = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(os.path.dirname(__file__), 'test.db'),
+#             'TEST_NAME': os.path.join(os.path.dirname(__file__), 'test.db'),
+#        }
+#     }
 
 
 def run():
