4504c6ddd3269956fbe1a87415931a83cf8ef0a5
FinalAngel <angelo.dini@divio.ch>
2015-03-30 22:03:25 +0200

    separate js into its own file

---
+++ aldryn_events/boilerplates/bootstrap3/static/js/cl.events.js
@@ -0,0 +1,86 @@
+/*!
+ * @author:    Divio AG
+ * @copyright: http://www.divio.ch
+ */
+
+//######################################################################################################################
+// #NAMESPACES#
+var Cl = window.Cl || {};
+
+//######################################################################################################################
+// #UTILS#
+(function ($) {
+    'use strict';
+
+    Cl.events = {
+
+        // INFO: autoinit certain functionalities
+        init: function () {
+            var that = this;
+
+            $('.js-events-calendar').each(function () {
+                that.calendar($(this));
+            });
+        },
+
+        // INFO: handle calendar
+        calendar: function (calendar) {
+            var settings = calendar.data();
+            var controls = calendar.find('.controls .js-trigger');
+
+            // attach events
+            controls.on('click', function (e) {
+                e.preventDefault();
+                var calendar = $(this).closest('.events-calendar');
+                var direction = $(this).data('direction');
+                var table = calendar.find('.table-calendar');
+                var month = parseInt(table.data('month-numeric'));
+                var year = parseInt(table.data('year'));
+                var title = calendar.find('h3');
+
+                // cancel if no direction is provided
+                if (!direction) {
+                    return false;
+                }
+
+                // handle first and last bound
+                if (direction === 'next') {
+                    if (month === 12) {
+                        month = 1;
+                        year += 1;
+                    } else {
+                        month += 1;
+                    }
+                }
+                if (direction === 'previous') {
+                    if (month === 1) {
+                        month = 12;
+                        year -= 1;
+                    } else {
+                        month -= 1;
+                    }
+                }
+
+                // send proper ajax request
+                $.ajax({
+                    'type': 'get',
+                    'url': settings.url + year + '/' + month + '/?plugin_pk=' + settings.pk,
+                    'success': function (data) {
+                        table.replaceWith(data);
+                        title.html($(data).data('month') + ' ' + $(data).data('year'));
+                    },
+                    'error': function () {
+                        alert(settings.error);
+                    }
+                });
+            });
+        }
+
+    };
+
+    // autoload
+    if ($('.js-events-calendar').length) {
+        Cl.events.init();
+    }
+
+})(jQuery);
