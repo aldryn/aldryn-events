e8969a370f04f01c4a94dbf0c05d819caa9c5ca0
Felipe Prenholato <philipe.rp@gmail.com>
2015-05-26 20:58:50 -0300

    Allow to use like: python runtests.py 'aldryn_events.tests.test_plugins.EventPluginsTestCase.test_event_list_plugin'

--- runtests.py
+++ runtests.py
@@ -4,8 +4,11 @@ import os
 
 import django
 
+_args = list(sys.argv)
+_args.pop(_args.index('runtests.py'))
 
-cmd = 'coverage run `which djangocms-helper` aldryn_events test --cms --extra-settings=test_settings'
+cmd = 'coverage run `which djangocms-helper` aldryn_events test --cms --extra-settings=test_settings %s'
+cmd = cmd % ' '.join(_args)
 
 if django.VERSION[:2] < (1, 6):
     cmd += ' --runner=discover_runner.DiscoverRunner'
