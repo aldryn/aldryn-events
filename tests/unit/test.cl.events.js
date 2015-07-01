feff30ab7922e37096a3de1acbff21cb582825b5
Angelo Dini <angelo.dini@divio.ch>
2015-07-01 13:30:06 +0200

    add test files

---
+++ tests/unit/test.cl.events.js
@@ -0,0 +1,22 @@
+/*!
+ * @author:    Divio AG
+ * @copyright: http://www.divio.ch
+ */
+
+// #####################################################################################################################
+// #NAMESPACES#
+// var Cl = window.Cl || {};
+/* global describe, it, expect */
+
+// #####################################################################################################################
+// #TESTS#
+(function () {
+    'use strict';
+
+    describe('cl.events.js', function () {
+        it('loads Cl', function () {
+            expect(Cl).toBeDefined();
+        });
+    });
+
+})();
