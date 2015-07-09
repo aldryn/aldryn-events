68768a38cae452d01b6a4127a3c68bbdf3be0ac1
Marketionist <marketionist@gmail.com>
2015-07-09 12:27:56 +0300

    Added page creation test

--- aldryn_events/tests/frontend/integration/pages/page.events.crud.js
+++ aldryn_events/tests/frontend/integration/pages/page.events.crud.js
@@ -12,39 +12,52 @@
 var eventsPage = {
     site: 'http://127.0.0.1:8000/en/',
     mainElementsWaitTime: 12000,
+    iframeWaitTime: 15000,
 
     // log in, log out
     editModeLink: element(by.css('.inner a[href="/?edit"]')),
     usernameInput: element(by.id('id_cms-username')),
     passwordInput: element(by.id('id_cms-password')),
     loginButton: element(by.css('.cms_form-login input[type="submit"]')),
-    userMenuOption: element.all(by.css(
-        '.cms_toolbar-item-navigation li')).first(),
+    userMenus: element.all(by.css('.cms_toolbar-item-navigation li')),
+    userMenuDropdown: element(by.css(
+        '.cms_toolbar-item-navigation-hover')),
+    administrationOptions: element.all(by.css(
+        '.cms_toolbar-item-navigation a[href="/en/admin/"]')),
+    sideMenuIframe: element(by.css('.cms_sideframe-frame iframe')),
+    addPageLink: element(by.css('.model-page .addlink')),
+    pageTitleInput: element(by.id('id_title')),
+    saveButton: element(by.css('.submit-row [name="_save"]')),
+    editPageLink: element(by.css('.col1 a')),
+    testLink: element(by.css('.selected a')),
 
-    cmsLogin: function (object) {
+    cmsLogin: function (credentials) {
         // object can contain username and password, if not set it will
         // fallback to 'admin'
-        object = object || {};
+        credentials = credentials ||
+            { username: 'admin', password: 'admin' };
 
         eventsPage.usernameInput.clear();
 
         // fill in email field
-        eventsPage.usernameInput.sendKeys(object.username || 'admin')
-            .then(function () {
+        eventsPage.usernameInput.sendKeys(
+            credentials.username || 'admin').then(function () {
             eventsPage.passwordInput.clear();
 
             // fill in password field
-            eventsPage.passwordInput.sendKeys(object.password || 'admin');
+            eventsPage.passwordInput.sendKeys(
+                credentials.password || 'admin');
         }).then(function () {
             eventsPage.loginButton.click();
 
             // wait for user menu to appear
             browser.wait(function () {
-                return browser.isElementPresent(eventsPage.userMenuOption);
+                return browser.isElementPresent(eventsPage.userMenus.first());
             }, eventsPage.mainElementsWaitTime);
 
             // validate user menu
-            expect(eventsPage.userMenuOption.isDisplayed()).toBeTruthy();
+            expect(eventsPage.userMenus.first().isDisplayed())
+                .toBeTruthy();
         });
     },
 
--- aldryn_events/tests/frontend/integration/specs/spec.events.crud.js
+++ aldryn_events/tests/frontend/integration/specs/spec.events.crud.js
@@ -4,13 +4,13 @@
  */
 
 'use strict';
-/* global describe, it, browser */
+/* global describe, it, browser, By, expect */
 
 // #############################################################################
 // INTEGRATION TEST
-describe('Aldryn Events tests: ', function () {
-    var eventsPage = require('../pages/page.events.crud.js');
+var eventsPage = require('../pages/page.events.crud.js');
 
+describe('Aldryn Events tests: ', function () {
     it('login to the site with valid username and password', function () {
         // go to the main page
         browser.get(eventsPage.site);
@@ -25,5 +25,59 @@ describe('Aldryn Events tests: ', function () {
 
         // login to the site
         eventsPage.cmsLogin();
+    });
+
+    it('create a new test page', function () {
+        // click the example.com link in the top menu
+        eventsPage.userMenus.first().click().then(function () {
+            // wait for top menu dropdown options to appear
+            browser.wait(function () {
+                return browser.isElementPresent(eventsPage.userMenuDropdown);
+            }, eventsPage.mainElementsWaitTime);
+
+            eventsPage.administrationOptions.first().click();
+        }).then(function () {
+            // wait for modal iframe to appear
+            browser.wait(function () {
+                return browser.isElementPresent(eventsPage.sideMenuIframe);
+            }, eventsPage.iframeWaitTime);
+
+            // switch to side menu iframe
+            browser.switchTo()
+                .frame(browser.findElement(By.css('.cms_sideframe-frame iframe')));
+
+            browser.wait(function () {
+                return browser.isElementPresent(eventsPage.addPageLink);
+            }, eventsPage.mainElementsWaitTime);
+
+            eventsPage.addPageLink.click();
+
+            browser.wait(function () {
+                return browser.isElementPresent(eventsPage.pageTitleInput);
+            }, eventsPage.mainElementsWaitTime);
+
+            eventsPage.pageTitleInput.sendKeys('Test');
+        }).then(function () {
+            eventsPage.saveButton.click();
+
+            browser.wait(function () {
+                return browser.isElementPresent(eventsPage.editPageLink);
+            }, eventsPage.mainElementsWaitTime);
+
+            eventsPage.editPageLink.click();
+
+            // switch to default page content
+            browser.switchTo().defaultContent();
+
+            browser.wait(function () {
+                return browser.isElementPresent(eventsPage.testLink);
+            }, eventsPage.mainElementsWaitTime);
+
+            // validate test link text
+            eventsPage.testLink.getText()
+            .then(function (title) {
+                expect(title).toEqual('Test');
+            });
+        });
     });
 });
