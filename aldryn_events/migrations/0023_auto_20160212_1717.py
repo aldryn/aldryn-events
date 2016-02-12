99063f009702f4629bec2882a0ba70efd6cb08f3
Mykhailo Kolesnyk <mykhailo.kolesnyk@divio.ch>
2016-02-12 19:26:15 +0200

    AppConfig: require application title

---
+++ aldryn_events/migrations/0023_auto_20160212_1717.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('aldryn_events', '0022_auto_20160109_1743'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='eventsconfigtranslation',
+            name='app_title',
+            field=models.CharField(default='', max_length=234, verbose_name='application title'),
+        ),
+        migrations.AlterField(
+            model_name='registration',
+            name='language_code',
+            field=models.CharField(default=b'en', max_length=32, choices=[(b'en', b'English')]),
+        ),
+    ]
